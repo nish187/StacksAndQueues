20ec71a815b577da9a77fa24a6fd2bd6591fa147
nisha <nishantsingh187@gmail.com>
2022-12-11 21:31:41 +0530

    Push To Stacks

---
+++ src/StacksAndQueues/Stack.java
@@ -0,0 +1,23 @@
+package StacksAndQueues;
+
+import BridgeLabz.*;
+
+public class Stack<T> {
+
+    LinkedList<T> linkedList;
+
+    public Stack() {
+        this.linkedList = new LinkedList<>();
+    }
+    public void push(T key) {
+        linkedList.push(key);
+    }
+
+    public void print() {
+        linkedList.display();
+    }
+
+    public T pop() {
+        return linkedList.pop();
+    }
+}
